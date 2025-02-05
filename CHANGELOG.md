8ab1e5153b19394e8582827579fc29695e1e6172
Andy <aejefferson@icloud.com>
2025-02-05 16:14:46 +0100

    release: v0.3.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.3.2] - 2024-03-19
+
+### Fixed
+- Fixed dynamic require of url module in ESM context
+- Updated import-meta-url.js to use proper ESM imports
+
 ## [0.3.1] - 2024-03-19
 
 ### Changed
--- src/import-meta-url.js
+++ src/import-meta-url.js
@@ -1,2 +1,7 @@
-// eslint-disable-next-line no-undef
-export var import_meta_url = require('url').pathToFileURL(__filename);
+import { pathToFileURL } from 'node:url';
+import { fileURLToPath } from 'node:url';
+
+// In ESM, __filename is not available, so we need to construct it
+const __filename = fileURLToPath(import.meta.url);
+
+export const import_meta_url = pathToFileURL(__filename);
